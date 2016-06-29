4b403a3a0f09715db2f4a1bfc2467b3fe76f9375
Stefan Koch <Uplink.Coder@googlemail.com>
2016-06-29 22:45:40 +0200

    add printInstrutions

--- src/ctfe_bc.d
+++ src/ctfe_bc.d
@@ -22,7 +22,7 @@ struct CtfeStack {
 	}
 }
 struct StackRef {
-	uint sp;
+	short sp;
 	Type type;
 }
 auto evaluateFunction(FuncDeclaration fd, Expression[] args, ThisExp _this = null) {
@@ -38,9 +38,9 @@ auto evaluateFunction(FuncDeclaration fd, Expression[] args, ThisExp _this = nul
 	if (auto fbody = fd.fbody.isCompoundStatement) {
 		bcv.visit(fbody);
 		debug { import std.stdio;
-			writeln(cast(ushort[])bcv.byteCodeArray[0 .. bcv.ip*2+3]);
+			bcv.printInstructions.writeln;
+			writeln("stackUsage = ", bcv.sp-4);
 		}
-
 	}
 	return null;
 }
@@ -64,9 +64,85 @@ extern(C++) final class BCV : Visitor {
 	// ip starts at 4 because 0 should be an invalid address;
 	BCValue retval;
 	BCAddr ip = BCAddr(4);
-	uint sp = 4;
+	short sp = 4;
+
+	ubyte unreslovedLabelCount;
 
 	bool discardValue = false;
+
+	/* 
+	 * 32bitInst : 
+	 * [0 .. 6] Instruction
+	 * [6 .. 9] Flags
+	 * 
+	 * [9 .. 16] (optional InstData)
+	 * [16 .. 33] 16BitOperand
+	 *
+	 *
+	 * 64BitInst :
+	 * [0 .. 6] Instruction
+	 * [6 .. 9] Flags (* Bit 6 is true for 64BitInst
+	 * 
+	 * 
+	 */ 
+
+	string printInstructions() {
+
+		string result = "StartInstructionDump: \n";
+		size_t pos = 4;
+		import std.conv;
+		auto arr = byteCodeArray;
+		auto length = ip - 4;
+		result ~= "Length : " ~ to!string(length) ~ "\n";
+
+		enum InstLengthMask = ubyte(0x20); // check 6th Byte
+		enum InstMask = ubyte(0x1F); // mask for bit 0-5 
+
+		while (length--) {
+			uint lw = arr[pos];
+			result ~= pos.to!string ~ ":\t";
+			++pos;
+			if (lw == 0) {
+				result ~= "0x0 0x0 0x0 0x0\n";
+				continue;
+			}
+
+			if (lw & InstLengthMask) {
+				// We have a long instruction
+
+				--length;
+				uint hi = arr[pos++];
+			//	LongInst instValue = );  
+				switch(cast(LongInst)(lw & InstMask)) {
+					case LongInst.ImmAdd : {
+						result ~= "Add SP[" ~ to!string(lw >> 16) ~ "], " ~ to!string(hi) ~ "\n"; 
+					} break;
+					case LongInst.Jmp : {
+						result ~= "Jmp &" ~ to!string(hi) ~ "\n"; 
+					} break;
+					default : {
+						result ~= "Unkown LongInst \n";
+					} break;
+				}
+			} else {
+				// We have a short instruction
+
+				switch(cast(ShortInst)(lw & InstMask)) {
+					case ShortInst.Ret : {
+						result ~= "Ret SP[" ~ to!string(lw >> 16) ~ "] \n"; 
+					} break;
+					case ShortInst.Jmp : {
+						result ~= "Jmp &" ~ to!string(cast(short)(lw >> 16) + pos - 1) ~ "\n"; 
+					} break;
+					default : {
+						result ~= "Unkown ShortInst \n";
+					} break;
+				}
+			}
+		}
+		return result ~ "\n EndInstructionDump";
+	} 
+
 
 	enum ShortInst : ubyte {
 
@@ -78,8 +154,11 @@ extern(C++) final class BCV : Visitor {
 		Div,
 	}
 
+	static assert(ShortInst.max < 0x20, "Instruction do not fit in 6 byte anymore");
+
 	enum LongInst : ushort {
 		Jmp,
+		Inc2,
 		ImmAdd,
 
 	}
@@ -88,26 +167,26 @@ extern(C++) final class BCV : Visitor {
 		uint lw;
 		uint hi;
 
-	this(LongInst i, uint imm) {
-			lw = 0b10 | i << 2 ;
+		this(LongInst i, uint imm) {
+			lw = i | 1 << 5;
 			hi = imm;
-	}
+		}
 
-	this(LongInst i, short stackAddr, uint imm) {
-		lw = 0b10 | i << 2 | stackAddr << 16;
-		hi = imm; 
+		this(LongInst i, short stackAddr, uint imm) {
+			lw = i | 1 << 5 | stackAddr << 16;
+			hi = imm; 
+		}
 	}
-}
 
 	static assert(ShortInst.max < 64);
 
 	uint ShortInst16(ShortInst i, short imm) {
-		return 0b00 | i << 2 | imm << 16; 
+		return i | imm << 16; 
 	}
 
 	uint ShortInst24(ShortInst i, uint imm) {
 		assert(imm == (imm & 0x0FFF));
-		return 0b00 | i << 2 | imm >> 8; 
+		return i | imm << 8; 
 	}
 
 	enum BCType {
@@ -157,7 +236,7 @@ extern(C++) final class BCV : Visitor {
 
 	struct BCValue {
 		BCValueType vType;
-		uint stackAddr;
+		short stackAddr;
 		BCType type;
 		union {
 			void* valAddr;
@@ -186,7 +265,7 @@ extern(C++) final class BCV : Visitor {
 		}
 
 
-		this(void* base, uint addr, BCType type) pure {
+		this(void* base, short addr, BCType type) pure {
 			this.stackAddr = addr;
 			this.type = type;
 			this.valAddr = base + addr;
@@ -203,6 +282,7 @@ extern(C++) final class BCV : Visitor {
 
 	struct BCLabel {
 		BCAddr addr;
+		ubyte unresolvedId;
 	}
 
 	struct BCExpr {
@@ -235,6 +315,11 @@ public :
 //			default : assert(0, "Unhandled op" ~ to!string(op));
 //		}
 //	}
+
+	BCLabel* unresolvedLabel() {
+		unresolvedLabels[unreslovedLabelCount] = BCLabel(BCAddr.init, unreslovedLabelCount);
+		return &unresolvedLabel[unreslovedLabelCount++];
+	}
 
 	void visit(VarDeclaration v, VisitationType vt) {
 		if (vt == VisitationType.asExpression) {
@@ -334,6 +419,10 @@ public :
 		assert(target.addr);
 		auto at = beginJmp();
 		endJmp(at, target);
+		debug {
+			import std.stdio;
+			"Calling genJump".writeln ;
+		}
 		return at;
 	}
 
@@ -375,6 +464,7 @@ public :
 		}
 
 		if (fs.condition !is null && fs._body !is null) {
+			BCLabel beginCond = genLabel();
 			BCExpr* cond = genExpr(fs.condition);
 			BCLabel afterBody;
 			auto branch = beginBranch();
@@ -383,6 +473,7 @@ public :
 				fs.increment.accept(this);
 				_body.end = genLabel();
 			}
+			genJump(beginCond);
 		//	endBranch(branch, cond.value, &cond.evalBlock.begin, &_body.end);
 		} else if (fs.condition !is null /* && fs._body is null*/) {
 			BCLabel beginCond = genLabel();
@@ -390,7 +481,8 @@ public :
 			if (fs.increment) {
 				fs.increment.accept(this);
 			}
-			genJumpIfTrue(condExpr, &beginCond);
+		//	genJumpIfTrue(condExpr, &beginCond);
+			genJump(beginCond);
 		} else { // fs.condition is null && fs._body !is null
 			auto _body = genBlock(fs._body);
 			if (fs.increment) {
@@ -398,7 +490,7 @@ public :
 			}
 			genJump(_body.begin);
 		}
-
+		
 	}
 
 	override void visit(Expression e) {
@@ -504,16 +596,20 @@ public :
 		assert(rhs.vType == BCValueType.Immidiate);
 		assert(rhs.type == BCType.i32 && lhs.type == BCType.i32);
 
-
-		byteCodeArray[ip] = 0b01 | LongInst.ImmAdd << 2 | lhs.stackAddr << 16;
-		byteCodeArray[ip + 1] = rhs.imm32;
+		debug {
+			import std.stdio;
+			writeln("emitAdd("~ ip.to!string ~ "|" ~  lhs.to!string ~ ", " ~ rhs.to!string ~ ")");
+		}
+		auto i = LongInst64(LongInst.ImmAdd, lhs.stackAddr, rhs.imm32);
+		byteCodeArray[ip] = i.lw;
+		byteCodeArray[ip + 1] = i.hi;
 		ip += 2;
 	}
 
 	void emitReturn(BCValue val) {
 		assert(val.vType == BCValueType.StackValue);
-		byteCodeArray[ip] = 0b00 | ShortInst.Ret << 2 | val.stackAddr << 16;
-		ip += 2;
+		byteCodeArray[ip] = ShortInst.Ret | val.stackAddr << 16;
+		ip += 1;
 	}
 
 	override void visit(IntegerExp ie) {
@@ -535,7 +631,6 @@ public :
 
 			ae.e1.toString().writeln;
 			ae.e2.toString().writeln;
-
 		}
 
 	}
@@ -545,7 +640,8 @@ public :
 			import std.stdio;
 			writefln("ReturnStatement %s", rs.toString);
 		}
-		(rs.exp.accept(this));
+
+		rs.exp.accept(this);
 		emitReturn(retval);
 	}
 
@@ -589,6 +685,4 @@ public :
 			else */{stmt.accept(this);}
 		}
 	}
-
-
 }
