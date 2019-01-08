b66c52be06baf8d949120ba56f1c1c60436fc7db
Stefan Koch <uplink.coder@googlemail.com>
2019-01-08 20:28:02 +0100

    Update fpconv_ctfe

--- src/ctfe/fpconv_ctfe.d
+++ src/ctfe/fpconv_ctfe.d
@@ -12,67 +12,154 @@ enum firstpower = -348; /* 10 ^ -348 */
 enum expmax = -32;
 enum expmin = -60;
 
+enum expbits = 11; // 64 - 1 /*for signbit*/ - fracbits;
+enum fracbits = 52;
+
+enum fracmask = (1UL << fracbits) - 1;
+enum hiddenbit =  (1UL << fracbits);
+enum expmask = ((1UL << expbits) - 1) << fracbits;
+enum signbit = (1UL << (double.sizeof * 8) -1); // 1 << (64 - 1)
+enum expbias = (((1 << (expbits - 1)) -1) + fracbits);
+
+static assert(fracbits + expbits + 1 == (double.sizeof * 8));
+
+
 struct Fp {
     ulong frac;
     int exp;
 };
 
-static immutable Fp[npowers] powers_ten = [
-    { 18054884314459144840U, -1220 }, { 13451937075301367670U, -1193 },
-    { 10022474136428063862U, -1166 }, { 14934650266808366570U, -1140 },
-    { 11127181549972568877U, -1113 }, { 16580792590934885855U, -1087 },
-    { 12353653155963782858U, -1060 }, { 18408377700990114895U, -1034 },
-    { 13715310171984221708U, -1007 }, { 10218702384817765436U, -980 },
-    { 15227053142812498563U, -954 }, { 11345038669416679861U, -927 },
-    { 16905424996341287883U, -901 }, { 12595523146049147757U, -874 },
-    { 9384396036005875287U, -847 }, { 13983839803942852151U, -821 },
-    { 10418772551374772303U, -794 }, { 15525180923007089351U, -768 },
-    { 11567161174868858868U, -741 }, { 17236413322193710309U, -715 },
-    { 12842128665889583758U, -688 }, { 9568131466127621947U, -661 },
-    { 14257626930069360058U, -635 }, { 10622759856335341974U, -608 },
-    { 15829145694278690180U, -582 }, { 11793632577567316726U, -555 },
-    { 17573882009934360870U, -529 }, { 13093562431584567480U, -502 },
-    { 9755464219737475723U, -475 }, { 14536774485912137811U, -449 },
-    { 10830740992659433045U, -422 }, { 16139061738043178685U, -396 },
-    { 12024538023802026127U, -369 }, { 17917957937422433684U, -343 },
-    { 13349918974505688015U, -316 }, { 9946464728195732843U, -289 },
-    { 14821387422376473014U, -263 }, { 11042794154864902060U, -236 },
-    { 16455045573212060422U, -210 }, { 12259964326927110867U, -183 },
-    { 18268770466636286478U, -157 }, { 13611294676837538539U, -130 },
-    { 10141204801825835212U, -103 }, { 15111572745182864684U, -77 },
-    { 11258999068426240000U, -50 }, { 16777216000000000000U, -24 },
-    { 12500000000000000000U, 3 }, { 9313225746154785156U, 30 },
-    { 13877787807814456755U, 56 }, { 10339757656912845936U, 83 },
-    { 15407439555097886824U, 109 }, { 11479437019748901445U, 136 },
-    { 17105694144590052135U, 162 }, { 12744735289059618216U, 189 },
-    { 9495567745759798747U, 216 }, { 14149498560666738074U, 242 },
-    { 10542197943230523224U, 269 }, { 15709099088952724970U, 295 },
-    { 11704190886730495818U, 322 }, { 17440603504673385349U, 348 },
-    { 12994262207056124023U, 375 }, { 9681479787123295682U, 402 },
-    { 14426529090290212157U, 428 }, { 10748601772107342003U, 455 },
-    { 16016664761464807395U, 481 }, { 11933345169920330789U, 508 },
-    { 17782069995880619868U, 534 }, { 13248674568444952270U, 561 },
-    { 9871031767461413346U, 588 }, { 14708983551653345445U, 614 },
-    { 10959046745042015199U, 641 }, { 16330252207878254650U, 667 },
-    { 12166986024289022870U, 694 }, { 18130221999122236476U, 720 },
-    { 13508068024458167312U, 747 }, { 10064294952495520794U, 774 },
-    { 14996968138956309548U, 800 }, { 11173611982879273257U, 827 },
-    { 16649979327439178909U, 853 }, { 12405201291620119593U, 880 },
-    { 9242595204427927429U, 907 }, { 13772540099066387757U, 933 },
-    { 10261342003245940623U, 960 }, { 15290591125556738113U, 986 },
-    { 11392378155556871081U, 1013 }, { 16975966327722178521U, 1039 },
-    { 12648080533535911531U, 1066 }
-];
+enum seperate_arrays = 1;
+
+static if (seperate_arrays)
+{
+
+    static immutable int[npowers] powers_ten_exps =
+    [
+        -1220, -1193, -1166, -1140,
+        -1113, -1087, -1060, -1034,
+        -1007, -980, -954, -927,
+        -901, -874, -847, -821,
+        -794, -768, -741, -715,
+        -688, -661, -635, -608,
+        -582, -555, -529, -502,
+        -475, -449, -422, -396,
+        -369, -343, -316, -289,
+        -263, -236, -210, -183,
+        -157, -130, -103, -77,
+        -50, -24, 3, 30, 56,
+        83, 109, 136, 162,
+        189, 216, 242, 269,
+        295, 322, 348, 375,
+        402, 428, 455, 481,
+        508, 534, 561, 588,
+        614, 641, 667, 694,
+        720, 747, 774, 800,
+        827, 853, 880, 907,
+        933, 960, 986, 1013,
+        1039, 1066
+    ];
+
+    static immutable ulong[npowers] powers_ten_fracs =
+    [
+        18054884314459144840LU, 13451937075301367670LU, 10022474136428063862LU,
+        14934650266808366570LU, 11127181549972568877LU, 16580792590934885855LU,
+        12353653155963782858LU, 18408377700990114895LU, 13715310171984221708LU,
+        10218702384817765436LU, 15227053142812498563LU, 11345038669416679861LU,
+        16905424996341287883LU, 12595523146049147757LU, 9384396036005875287LU,
+        13983839803942852151LU, 10418772551374772303LU, 15525180923007089351LU,
+        11567161174868858868LU, 17236413322193710309LU, 12842128665889583758LU,
+        9568131466127621947LU, 14257626930069360058LU, 10622759856335341974LU,
+        15829145694278690180LU, 11793632577567316726LU, 17573882009934360870LU,
+        13093562431584567480LU, 9755464219737475723LU, 14536774485912137811LU,
+        10830740992659433045LU, 16139061738043178685LU, 12024538023802026127LU,
+        17917957937422433684LU, 13349918974505688015LU, 9946464728195732843LU,
+        14821387422376473014LU, 11042794154864902060LU, 16455045573212060422LU,
+        12259964326927110867LU, 18268770466636286478LU, 13611294676837538539LU,
+        10141204801825835212LU, 15111572745182864684LU, 11258999068426240000LU,
+        16777216000000000000LU, 12500000000000000000LU, 9313225746154785156LU,
+        13877787807814456755LU, 10339757656912845936LU, 15407439555097886824LU,
+        11479437019748901445LU, 17105694144590052135LU, 12744735289059618216LU,
+        9495567745759798747LU, 14149498560666738074LU, 10542197943230523224LU,
+        15709099088952724970LU, 11704190886730495818LU, 17440603504673385349LU,
+        12994262207056124023LU, 9681479787123295682LU, 14426529090290212157LU,
+        10748601772107342003LU, 16016664761464807395LU, 11933345169920330789LU,
+        17782069995880619868LU, 13248674568444952270LU, 9871031767461413346LU,
+        14708983551653345445LU, 10959046745042015199LU, 16330252207878254650LU,
+        12166986024289022870LU, 18130221999122236476LU, 13508068024458167312LU,
+        10064294952495520794LU, 14996968138956309548LU, 11173611982879273257LU,
+        16649979327439178909LU, 12405201291620119593LU, 9242595204427927429LU,
+        13772540099066387757LU, 10261342003245940623LU, 15290591125556738113LU,
+        11392378155556871081LU, 16975966327722178521LU, 12648080533535911531LU
+    ];
+}
+else
+{
+    static immutable Fp[npowers] powers_ten = [
+        { 18054884314459144840U, -1220 }, { 13451937075301367670U, -1193 },
+        { 10022474136428063862U, -1166 }, { 14934650266808366570U, -1140 },
+        { 11127181549972568877U, -1113 }, { 16580792590934885855U, -1087 },
+        { 12353653155963782858U, -1060 }, { 18408377700990114895U, -1034 },
+        { 13715310171984221708U, -1007 }, { 10218702384817765436U, -980 },
+        { 15227053142812498563U, -954 }, { 11345038669416679861U, -927 },
+        { 16905424996341287883U, -901 }, { 12595523146049147757U, -874 },
+        { 9384396036005875287U, -847 }, { 13983839803942852151U, -821 },
+        { 10418772551374772303U, -794 }, { 15525180923007089351U, -768 },
+        { 11567161174868858868U, -741 }, { 17236413322193710309U, -715 },
+        { 12842128665889583758U, -688 }, { 9568131466127621947U, -661 },
+        { 14257626930069360058U, -635 }, { 10622759856335341974U, -608 },
+        { 15829145694278690180U, -582 }, { 11793632577567316726U, -555 },
+        { 17573882009934360870U, -529 }, { 13093562431584567480U, -502 },
+        { 9755464219737475723U, -475 }, { 14536774485912137811U, -449 },
+        { 10830740992659433045U, -422 }, { 16139061738043178685U, -396 },
+        { 12024538023802026127U, -369 }, { 17917957937422433684U, -343 },
+        { 13349918974505688015U, -316 }, { 9946464728195732843U, -289 },
+        { 14821387422376473014U, -263 }, { 11042794154864902060U, -236 },
+        { 16455045573212060422U, -210 }, { 12259964326927110867U, -183 },
+        { 18268770466636286478U, -157 }, { 13611294676837538539U, -130 },
+        { 10141204801825835212U, -103 }, { 15111572745182864684U, -77 },
+        { 11258999068426240000U, -50 }, { 16777216000000000000U, -24 },
+        { 12500000000000000000U, 3 }, { 9313225746154785156U, 30 },
+        { 13877787807814456755U, 56 }, { 10339757656912845936U, 83 },
+        { 15407439555097886824U, 109 }, { 11479437019748901445U, 136 },
+        { 17105694144590052135U, 162 }, { 12744735289059618216U, 189 },
+        { 9495567745759798747U, 216 }, { 14149498560666738074U, 242 },
+        { 10542197943230523224U, 269 }, { 15709099088952724970U, 295 },
+        { 11704190886730495818U, 322 }, { 17440603504673385349U, 348 },
+        { 12994262207056124023U, 375 }, { 9681479787123295682U, 402 },
+        { 14426529090290212157U, 428 }, { 10748601772107342003U, 455 },
+        { 16016664761464807395U, 481 }, { 11933345169920330789U, 508 },
+        { 17782069995880619868U, 534 }, { 13248674568444952270U, 561 },
+        { 9871031767461413346U, 588 }, { 14708983551653345445U, 614 },
+        { 10959046745042015199U, 641 }, { 16330252207878254650U, 667 },
+        { 12166986024289022870U, 694 }, { 18130221999122236476U, 720 },
+        { 13508068024458167312U, 747 }, { 10064294952495520794U, 774 },
+        { 14996968138956309548U, 800 }, { 11173611982879273257U, 827 },
+        { 16649979327439178909U, 853 }, { 12405201291620119593U, 880 },
+        { 9242595204427927429U, 907 }, { 13772540099066387757U, 933 },
+        { 10261342003245940623U, 960 }, { 15290591125556738113U, 986 },
+        { 11392378155556871081U, 1013 }, { 16975966327722178521U, 1039 },
+        { 12648080533535911531U, 1066 }
+    ];
+}
+
 
 Fp find_cachedpow10(int exp, int* k)
 {
-    const double one_log_ten = 0.30102999566398114;
+    enum one_log_ten = 0.30102999566398114;
 
     int approx = cast(int) (-(exp + npowers) * one_log_ten);
-    int idx = (approx - firstpower) / 8;
+    int idx = (approx - firstpower) / steppowers;
 
     while(1) {
-        int current = exp + powers_ten[idx].exp + 64;
+        static if (seperate_arrays)
+        {
+            int current = exp + powers_ten_exps[idx] + 64;
+        }
+        else
+        {
+            int current = exp + powers_ten[idx].exp + 64;
+        }
 
         if(current < expmin) {
             idx++;
@@ -86,7 +173,16 @@ Fp find_cachedpow10(int exp, int* k)
 
         *k = (firstpower + idx * steppowers);
 
-        return powers_ten[idx];
+        static if (seperate_arrays)
+        {
+            auto result = Fp(powers_ten_fracs[idx], powers_ten_exps[idx]);
+        }
+        else
+        {
+            auto result = powers_ten[idx];
+        }
+
+        return result;
     }
 }
 
@@ -102,7 +198,7 @@ static immutable ulong[20] tens = [
 
 ulong get_dbits(double d)
 {
-    ulong bits = *cast(ulong*)&d;
+    const bits = *cast(ulong*)&d;
     return bits;
 }
 
@@ -111,23 +207,41 @@ Fp build_fp(double d)
     ulong bits = get_dbits(d);
 
     Fp fp;
-    fp.frac = bits & 0x000FFFFFFFFFFFFFU;
-    fp.exp = (bits & 0x7FF0000000000000U) >> 52;
+    fp.frac = bits & fracmask;
+    fp.exp = (bits & expmask) >> fracbits;
 
     if(fp.exp) {
-        fp.frac += 0x0010000000000000U;
-        fp.exp -= (1023 + 52);
-
+        fp.frac += hiddenbit;
+        fp.exp -= expbias;
     } else {
-        fp.exp = -(1023 + 52) + 1;
+        fp.exp = -(expbias) + 1;
     }
 
     return fp;
 }
 
+double build_double(Fp fp)
+{
+  ulong bits;
+  if (fp.exp == -(expbias) + 1)
+  {
+      bits = fp.frac;
+  }
+  else
+  {
+     bits = fp.frac - hiddenbit;
+     bits |= ((ulong(fp.exp + expbias) << fracbits) & expmask); 
+  }
+
+  double r = *(cast(double*) &bits);
+  return r; 
+}
+
+static assert (build_fp(double.max).build_double == double.max);
+
 void normalize(Fp* fp)
 {
-    while ((fp.frac & 0x0010000000000000U) == 0) {
+    while ((fp.frac & hiddenbit) == 0) {
         fp.frac <<= 1;
         fp.exp--;
     }
@@ -142,7 +256,7 @@ void get_normalized_boundaries(Fp* fp, Fp* lower, Fp* upper)
     upper.frac = (fp.frac << 1) + 1;
     upper.exp = fp.exp - 1;
 
-    while ((upper.frac & (0x0010000000000000U << 1)) == 0) {
+    while ((upper.frac & (hiddenbit << 1)) == 0) {
         upper.frac <<= 1;
         upper.exp--;
     }
@@ -163,9 +277,9 @@ void get_normalized_boundaries(Fp* fp, Fp* lower, Fp* upper)
     lower.exp = upper.exp;
 }
 
-Fp multiply(Fp* a, Fp* b)
+Fp multiply(const Fp* a, const Fp* b)
 {
-    const ulong lomask = 0x00000000FFFFFFFF;
+    enum lomask = 0x00000000FFFFFFFF;
 
     ulong ah_bl = (a.frac >> 32) * (b.frac & lomask);
     ulong al_bh = (a.frac & lomask) * (b.frac >> 32);
@@ -176,10 +290,10 @@ Fp multiply(Fp* a, Fp* b)
 
     tmp += 1U << 31;
 
-    Fp fp = {
-        ah_bh + (ah_bl >> 32) + (al_bh >> 32) + (tmp >> 32),
-        a.exp + b.exp + 64
-    };
+    ulong fract = ah_bh + (ah_bl >> 32) + (al_bh >> 32) + (tmp >> 32);
+    int exp = a.exp + b.exp + 64;
+
+    auto fp = Fp (fract, exp);
 
     return fp;
 }
@@ -189,7 +303,7 @@ void round_digit(char* digits, int ndigits, ulong delta, ulong rem, ulong kappa,
     while (rem < frac && delta - rem >= kappa &&
            (rem + kappa < frac || frac - rem > rem + kappa - frac)) {
 
-        digits[ndigits - 1]--;
+        --digits[ndigits - 1];
         rem += kappa;
     }
 }
@@ -284,9 +398,9 @@ static int emit_digits(char* digits, int ndigits, char* dest, int K, bool neg)
 
 
     if(K >= 0 && (exp < (ndigits + 7))) {
-        dest[0 .. ndigits] = digits[0 .. ndigits]; 
+        dest[0 .. ndigits] = digits[0 .. ndigits];
         //memcpy(dest, digits, ndigits);
-        dest[ndigits .. ndigits + K] =  0;
+        dest[ndigits .. ndigits + K] =  '0';
         //memset(dest + ndigits, '0', K);
 
         return ndigits + K;
@@ -302,7 +416,7 @@ static int emit_digits(char* digits, int ndigits, char* dest, int K, bool neg)
             dest[1] = '.';
 
             //memset(dest + 2, '0', offset);
-            dest[2 .. 2 + offset] = 0;
+            dest[2 .. 2 + offset] = '0';
             //memcpy(dest + offset + 2, digits, ndigits);
             dest[2 + offset .. 2 + offset + ndigits] = digits[0 .. ndigits];
 
@@ -314,7 +428,7 @@ static int emit_digits(char* digits, int ndigits, char* dest, int K, bool neg)
             dest[0 .. offset] = digits[0 .. offset];
             dest[offset] = '.';
             //memcpy(dest + offset + 1, digits + offset, ndigits - offset);
-            dest[offset + 1 .. 1 + ndigits] = (digits + offset)[0 .. ndigits - offset];
+            dest[offset + 1 .. 1 + ndigits] = digits[offset .. ndigits];
 
             return ndigits + 1;
         }
@@ -361,20 +475,21 @@ static int emit_digits(char* digits, int ndigits, char* dest, int K, bool neg)
 /+
 static int filter_special(double fp, char* dest)
 {
-    if(fp == 0.0) {
+    ulong bits = get_dbits(fp);
+
+    if((bits & ~signbit) == 0) {
         dest[0] = '0';
         return 1;
     }
 
-    ulong bits = get_dbits(fp);
 
-   bool nan = (bits & 0x7FF0000000000000U) == 0x7FF0000000000000U;
+    bool nan = (bits & expmask) == expmask;
 
     if(!nan) {
         return 0;
     }
 
-    if(bits & 0x000FFFFFFFFFFFFFU) {
+    if(bits & fracmask) {
         dest[0] = 'n'; dest[1] = 'a'; dest[2] = 'n';
 
     } else {
@@ -384,40 +499,42 @@ static int filter_special(double fp, char* dest)
     return 3;
 }
 +/
-int fpconv_dtoa(double d, ref char[24] dest)
+int fpconv_dtoa(double d, /*ref char[24]*/ char* dest)
 {
     char[18] digits;
     int str_len = 0;
     bool neg = 0;
     ulong bits = (*(cast(ulong*)&d));
-    bool is_nan = ((bits & 0x7FF0000000000000U) == 0x7FF0000000000000U);
-    
-    if(bits & (1UL << 63)) {
+
+
+    if(bits & signbit) {
         dest[0] = '-';
         str_len = 1;
         neg = 1;
     }
 
-    if (d == 0.0)
+    if ((bits & ~signbit) == 0)
     {
-       dest[0] = '0';
-       return 1;
+       dest[str_len] = '0';
+       return ++str_len;
     }
 
+   // manually inlined is_special 
 
-    if (is_nan) 
+    bool is_nan = ((bits & expmask) == expmask);
+   
+    if (is_nan)
     {
         // this case is unlikely
-        // we are going to define it as delegate
-        // to give the compiler a chance to move it out
-        () {
+        // add branch hint when avilable
+        {
             str_len = 3;
             if (bits & 0x000FFFFFFFFFFFFFU)
             {
                 dest[0 .. 3] = "nan";
             }
             else
-            {  
+            {
                 if (neg)
                 {
                     dest[1 .. 4] = "inf";
@@ -426,17 +543,17 @@ int fpconv_dtoa(double d, ref char[24] dest)
                 else
                 {
                     dest[0 .. 3] = "inf";
-                }                
+                }
             }
-        } ();
-        
+        }
+
         return str_len;
     }
 
     int K = 0;
     auto ndigits = grisu2(d, &digits[0], &K);
-  
-    str_len += emit_digits(&digits[0], ndigits, &dest[0] + str_len, K, neg);
+
+    str_len += emit_digits(&digits[0], ndigits, &dest[str_len], K, neg);
     return str_len;
 }
 
@@ -444,17 +561,15 @@ int fpconv_dtoa(double d, ref char[24] dest)
 string fpconv_dtoa(double d)
 {
     char[24] buffer;
-    const len = fpconv_dtoa(d, buffer);
+    const len = fpconv_dtoa(d, &buffer[0]);
     auto result = new char[](len);
     result[0 .. len] = buffer[0 .. len];
     return cast(string) result;
 }
 
-static assert (() {
-    char[24] buffer;
-    int len = fpconv_dtoa(3.14159, buffer);
-    return buffer[0 .. len].idup;
-} () == "3.14159");
+static assert(() {
+    return fpconv_dtoa(3.14159);
+} ()  == "3.14159");
 
 static assert (fpconv_dtoa(0.3) == "0.3");
 static assert (fpconv_dtoa(-double.infinity) == "-inf");
@@ -464,5 +579,36 @@ static assert (fpconv_dtoa(1.3f) == "1.3");
 static assert (fpconv_dtoa(65.221) == "65.221");
 static assert (fpconv_dtoa(1.3) == "1.3");
 static assert (fpconv_dtoa(0.3) == "0.3");
+static assert (fpconv_dtoa(10) == "10");
+static assert (fpconv_dtoa(double.max) == "1.7976931348623157e+308");
 
+// printf can't handle this one ;)
+static assert (fpconv_dtoa(0.3049589) == "0.3049589");
 
+/+
+pragma(msg, () {
+    string[] result;
+    result.length = npowers * 2;
+    foreach(idx; 0 .. npowers * 2)
+    {
+        const idx2 = idx / 2;
+        if (idx & 1)
+        {
+            static if (seperate_arrays)
+            {
+                auto fp = Fp(powers_ten_fracs[idx2], powers_ten_exps[idx2]);
+            }
+            else
+            {
+                auto fp = powers_ten[idx2];
+            }
+            result[idx] = fpconv_dtoa(build_double(fp));
+        }
+        else
+        {
+            result[idx] = fpconv_dtoa(idx2);
+        }
+    }
+    return result;
+} ());
++/
