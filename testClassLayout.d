2f6e034dcc31935dc349f34f96d56e3cfc0334be
Stefan Koch <uplink.coder@googlemail.com>
2019-02-01 20:17:10 +0100

    include double in the test

--- testClassLayout.d
+++ testClassLayout.d
@@ -1,21 +1,21 @@
 
 alias twice = long; alias once = int;
-class B {once more = 0x123; twice the_fun = 0xFFF;}
+class B {once more = 0x123; twice the_fun = 0xFFF; double the_trouble = 0x666;}
 
 class C : B
 {
   int a = 12; int b = 13; int c = 14;
 
-  final int[5] toArray()
+  final auto toArray()
   {
-    int[5] arr;
-    arr = [a, b, c, cast(int) more, cast(int) the_fun];
+    int[6] arr;
+    arr = [a, b, c, cast(int) more, cast(int) the_fun, cast(int) the_trouble];
     return arr;
   }
 
 }
 
-immutable int[5] expectedData = [12, 13, 14, 0x123, 0xFFF];
+immutable int[6] expectedData = [12, 13, 14, 0x123, 0xFFF, 0x666];
 
 auto testLayout()
 {
@@ -23,8 +23,7 @@ auto testLayout()
     // currently default ctors don't work
     // therefore we need to do this by hand
     c.a = 12; c.b = 13; c.c = 14;
-    c.the_fun = 0xFFF;
-    c.more = 0x123;
+    c.more = 0x123; c.the_fun = 0xFFF; c.the_trouble = 0x666;
 
     return c.toArray;
 }
