ab298069322c23c1cb210d4d26880ae283b71e61
Stefan Koch <uplink.coder@googlemail.com>
2020-04-05 14:24:47 +0200

    Introduce the alias type

--- src/dmd/mtype.d
+++ src/dmd/mtype.d
@@ -248,7 +248,11 @@ enum ENUMTY : int
 
     Tdelegate,
     Tnone,
+
+    Talias, /// Type type
+
     Tvoid,
+
     Tint8,
     Tuns8,
     Tint16,
@@ -256,8 +260,8 @@ enum ENUMTY : int
     Tint32,
     Tuns32,
     Tint64,
-
     Tuns64,
+
     Tfloat32,
     Tfloat64,
     Tfloat80,
@@ -300,6 +304,7 @@ alias Tstruct = ENUMTY.Tstruct;
 alias Tenum = ENUMTY.Tenum;
 alias Tdelegate = ENUMTY.Tdelegate;
 alias Tnone = ENUMTY.Tnone;
+alias Talias = ENUMTY.Talias;
 alias Tvoid = ENUMTY.Tvoid;
 alias Tint8 = ENUMTY.Tint8;
 alias Tuns8 = ENUMTY.Tuns8;
@@ -409,6 +414,7 @@ extern (C++) abstract class Type : ASTNode
 
     type* ctype;                    // for back end
 
+    extern (C++) __gshared Type talias; 
     extern (C++) __gshared Type tvoid;
     extern (C++) __gshared Type tint8;
     extern (C++) __gshared Type tuns8;
@@ -818,6 +824,7 @@ extern (C++) abstract class Type : ASTNode
         // Set basic types
         __gshared TY* basetab =
         [
+            Talias,
             Tvoid,
             Tint8,
             Tuns8,
@@ -852,6 +859,8 @@ extern (C++) abstract class Type : ASTNode
             basic[basetab[i]] = t;
         }
         basic[Terror] = new TypeError();
+
+        talias = basic[Talias];
 
         tvoid = basic[Tvoid];
         tint8 = basic[Tint8];
@@ -3036,7 +3045,11 @@ extern (C++) final class TypeBasic : Type
         uint flags = 0;
         switch (ty)
         {
-        case Tvoid:
+        case Talias:
+            d = Token.toChars(TOK.alias_);
+            break;
+			
+		case Tvoid:
             d = Token.toChars(TOK.void_);
             break;
 
@@ -6812,4 +6825,19 @@ bool isCopyable(const Type t) pure nothrow @nogc
             return false;
     }
     return true;
+}
+
+/***************************************************
+ * Determine if type t is a Talias on some level.
+ * Params:
+ *      t = type to check
+ * Returns:
+ *      true if it's a Talias (a type type)
+ */
+static bool isAliasType(Type t)
+{
+    if (t.ty == Talias)
+        return true;
+    auto Tnext = t.nextOf();
+    return Tnext ? isAliasType(Tnext) : false;
 }
--- src/dmd/parse.d
+++ src/dmd/parse.d
@@ -3585,6 +3585,10 @@ final class Parser(AST) : Lexer
         //printf("parseBasicType()\n");
         switch (token.value)
         {
+        case TOK.alias_ :
+            t = AST.Type.talias;
+            goto LabelX;
+
         case TOK.void_:
             t = AST.Type.tvoid;
             goto LabelX;
