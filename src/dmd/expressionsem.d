702395450aac13678ecf04d19765b2a4628c8f77
Stefan Koch <uplink.coder@googlemail.com>
2020-04-19 00:51:02 +0200

    Break some eggs and make an omelette

--- src/dmd/expressionsem.d
+++ src/dmd/expressionsem.d
@@ -5166,7 +5166,7 @@ private extern (C++) final class ExpressionSemanticVisitor : Visitor
             if (dotId.ident == Id._tupleof)
             {
                 // we need to let the tupleof expand
-                src = expressionSemantic(e, sc);
+                // src = expressionSemantic(e, sc);
             }
         }
 
@@ -5668,6 +5668,7 @@ private extern (C++) final class ExpressionSemanticVisitor : Visitor
 
                 if (!tupLen)
                 {
+                    printf("tupLen Zero!\n");
                     return ExpandResult(n, null);
                 }
 
--- test/compilable/dotdotdot.d
+++ test/compilable/dotdotdot.d
@@ -12,6 +12,7 @@ struct S
 {
    int a;
    char c;
+   int h;
 }
 
 struct S1
@@ -21,28 +22,33 @@ struct S1
 
 alias structs = Seq!(S, S1);
 
-static assert([structs.sizeof...] == [5, 4]);
+static assert([structs.sizeof...] == [12, 4]);
 static assert([__traits(identifier, structs)...] == ["S", "S1"]);
-//pragma(msg, ((structs.tupleof)...).stringof...);
+// pragma(msg, [structs.tupleof....stringof...]); 
 
 struct MetaInfo
 {
     string[] struct_names;
-    string[][] struct_members;
+//    string[][] struct_members;
     uint[]     number_of_members;  
     size_t[] struct_size;
 }
-pragma(msg, "(structs.tupleof...)", (structs.tupleof...).stringof);
+// pragma(msg, "(structs.tupleof...)", (structs.tupleof...).stringof);
 
 static immutable info = MetaInfo(   [ __traits(identifier, structs)...],
-                                    [ [(structs.tupleof...).stringof]...],
+//                                    [ [ (structs.tupleof...).stringof]...],
                                     [ structs.tupleof.length... ],
                                     [structs.sizeof...]  ); 
 pragma(msg, info);
 
+static assert(__ArrayEq(info.struct_names, ["S", "S1"]));  // can't do == because life's not nice
+static assert(info.number_of_members == [3,1]);
+static assert(info.struct_size == [12,4]);
 
-static assert ([(SC.tupleof.sizeof...)] == 
+/+ broken by not calling semantic directly on tupleof...
+static assert ([(SC.tupleof....sizeof...)] == 
                                [1,      1,              1,      1,      4]);
-static assert ([(SC.tupleof.stringof...)] == 
+static assert ([(SC.tupleof....stringof...)] == 
                                   ["x",        "y",         "z",    "w",     "f"]);
 extern(C) align(4) struct SC { char x = 4; char y = 12; char z; char w; float f = 84.3; }
++/
