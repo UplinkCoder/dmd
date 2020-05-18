ee14a1308bde4e442d8ddaebf0ef749efaef7e24
Stefan Koch <uplink.coder@googlemail.com>
2020-05-18 10:16:46 +0200

    Allow alias parameters to match anything

--- src/dmd/mtype.d
+++ src/dmd/mtype.d
@@ -4840,7 +4840,16 @@ extern (C++) final class TypeFunction : TypeNext
                             }
                         }
                         else
-                            m = arg.implicitConvTo(tprm);
+                        {
+                            if (tprm.ty == Talias)
+                            {
+                                m = MATCH.convert;
+                            }
+                            else
+                            {
+                                m = arg.implicitConvTo(tprm);
+                            }
+                        }
                     }
                     //printf("match %d\n", m);
                 }
