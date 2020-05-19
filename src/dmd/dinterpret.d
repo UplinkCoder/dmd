626181ce36c6c3bd082376ebd5ba68fb0f88e7cd
Stefan Koch <uplink.coder@googlemail.com>
2020-05-19 13:23:39 +0200

    Add debug printout to DotIdExp

--- src/dmd/dinterpret.d
+++ src/dmd/dinterpret.d
@@ -2893,6 +2893,11 @@ public:
 
     override void visit(DotIdExp die)
     {
+        debug (LOG)
+        {
+            printf("%s DotIdExp::interpret() %s\n", die.loc.toChars(), die.toChars());
+        }
+
         if (die.ident == Id.stringof)
         {
             auto e1 = interpretRegion(die.e1, istate);
