1f29d4b329d6e8833c22607ba836cdcd74463d07
Stefan Koch <uplink.coder@googlemail.com>
2021-03-16 15:41:50 +0000

    Printing of the taskGraph and begnning of background task support

--- src/dmd/taskgroup.d
+++ src/dmd/taskgroup.d
@@ -3,6 +3,75 @@ module dmd.taskgroup;
 import core.thread.fiber;
 import core.stdc.stdio;
 
+import core.thread.threadbase;
+import core.thread.osthread;
+
+
+Thread[4] unorderedBackgroudThreads;
+TaskQueue[4] unorderedBackgroudQueue;
+
+import core.sys.posix.pthread;
+
+align(16) struct TaskQueue
+{
+    shared align(16) uint queueLock;
+    shared align(16) uint currentTickit;
+
+    // these can only be touched when locked
+    __gshared Task*[64] tasks;
+    __gshared uint n_queue_full;
+
+    void addTaskToQueue(Task* task, uint queueID)
+    {
+        assert(task.queueID == 0, "trying task which is already in a queue");
+        assert(queueID != 0, "Queue 0 means unowned, therefore can't be used as valid QueueID");
+        import core.atomic /*: atomicFence, cas, atomicFetchAdd*/;
+
+        auto tickit = atomicFetchAdd(currentTickit, 1);
+        if (tickit == 0)
+        {
+            tickit = 1;
+        }
+
+        // lock is presumably  unowned
+        static immutable string __mmPause = "asm nothrow pure { rep; nop; }";
+    Lspin:
+
+        while (queueLock != 0 || n_queue_full == tasks.length) { mixin(__mmPause); }  // spin
+        if (atomicLoad(queueLock) == 0)
+        {
+            // try aquiring it
+            if (cas(&queueLock, 0, tickit))
+            {
+                // we have the lock
+                if (n_queue_full < tasks.length)
+                {
+                    tasks[n_queue_full++] = task;
+                    task.queueID = queueID;
+                    atomicFence();
+                }
+                atomicStore(queueLock, 0); // unlock
+            }
+            else
+                goto Lspin;
+        }
+    }
+}
+
+void initBackgroundThreads()
+{
+    foreach(i, ref t; unorderedBackgroudThreads)
+    {
+        t = new Thread(() {
+                for(;;)
+                {
+
+                }
+        });
+    }
+}
+
+
 size_t align16(const size_t n) pure
 {
     pragma(inline, true);
@@ -16,12 +85,22 @@ struct OriginInformation
     Task* originator;
 }
 
+void dispatchToBackgroudThread(Task* task)
+{
+    static shared size_t queueCounter = 0;
+    import core.atomic;
+    auto queueID = atomicFetchAdd(queueCounter, 1) % unorderedBackgroudQueue.length;
+    
+}
+
 struct Task
 {
     void* delegate (void*) fn;
     void* taskData;
     Task*[] children;
     size_t n_children_completed;
+
+    size_t queueID;
 
     TaskFiber currentFiber;
     bool hasCompleted;
@@ -92,7 +171,8 @@ struct TaskGroup
         tasks = ptr[0 .. n];
     }
     
-    Task* addTask(void* delegate (void*) fn, void* taskData, Task* originator = getCurrentTask(), size_t line = __LINE__, string file = __FILE__)
+    Task* addTask(void* delegate (void*) fn, void* taskData, bool background_task = false,
+        Task* originator = getCurrentTask(), size_t line = __LINE__, string file = __FILE__)
     {
         if (tasks.length <= n_used)
         {
@@ -109,6 +189,11 @@ struct TaskGroup
         *task = Task(fn, taskData);
         if (originator)
             originator.children ~= task;
+
+        if (background_task)
+        {
+            dispatchToBackgroudThread(task);
+        }
 
         //debug (task)
         {
@@ -221,6 +306,46 @@ struct TaskGroup
     }
 }
 
+string taskGraph(Task* task, Task* parent = null, int indent = 0)
+{
+
+    static char[] formatPtr(Task* t)
+    {
+        char[32] formatBuffer; // only used for toHex conversion of ptr
+
+        string result;
+
+        auto len = sprintf(formatBuffer.ptr, "\"%p\"", t);
+        return formatBuffer[0 .. len].dup;
+    }
+
+    string result;
+    if (parent is null)
+    {
+        result ~= "digraph Tasks {\n";
+    }
+
+    foreach(_; 0 .. indent++)
+    {
+        result ~= "\t";
+    }
+
+    result ~= formatPtr(parent) ~ " -> " ~ formatPtr(task) ~ "\n";
+
+    foreach(ref c;task.children)
+    {
+        result ~= taskGraph(c, task, indent);
+        //result ~= formatPtr(task) ~ " > " ~ formatPtr(c);
+    }
+
+    if (parent is null)
+    {
+        result ~= "\n}";
+    }
+
+    return result;
+}
+
 @("Test Originator")
 unittest
 {
@@ -228,17 +353,24 @@ unittest
     bool task2_completed = 0;
 
     size_t line = __LINE__;
-    TaskGroup tg = TaskGroup("tg1", 2);
+    TaskGroup tg = TaskGroup("tg1", 3);
     tg.addTask((void* x) { // line + 2
         auto task = tg.addTask((void* x) { // line + 3
             task2_completed = true;
             return null;
         }, x);
+        auto task2 = tg.addTask((void* x) { // line + 7
+            task2_completed = true;
+            return null;
+        }, x);
+        
         assert(task.originInfo.line == line + 3);
+        assert(task2.originInfo.line == line + 7);
         assert(task.originInfo.originator.originInfo.line == line + 2);
         task1_completed = true;
         return x;
     }, null);
+
     tg.awaitCompletionOfAllTasks();
 
     assert(task1_completed);
