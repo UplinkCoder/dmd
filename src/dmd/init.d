971ec8ad452e99b3441f8291b87ff43259265591
Stefan Koch <uplink.coder@googlemail.com>
2021-06-26 15:47:37 +0200

    Fix style

--- src/dmd/init.d
+++ src/dmd/init.d
@@ -56,7 +56,8 @@ extern (C++) class Initializer : ASTNode
     Loc loc;
     InitKind kind;
 
-    override DYNCAST dyncast() const nothrow pure {
+    override DYNCAST dyncast() const nothrow pure
+    {
         return DYNCAST.initializer;
     }
 
