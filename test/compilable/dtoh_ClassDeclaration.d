e49eced0d5865c9b390237a574a3aba8cd6d4a9a
Iain Buclaw <ibuclaw@gdcproject.org>
2021-06-28 13:59:37 +0200

    test: Fix dtoh tests to be compilable to object file

--- test/compilable/dtoh_ClassDeclaration.d
+++ test/compilable/dtoh_ClassDeclaration.d
@@ -135,6 +135,7 @@ public:
 class B : public A, public I1, public I2
 {
 public:
+    using A::bar;
     void foo();
     void bar();
 };
@@ -277,6 +278,7 @@ interface I2 : I1
 
 class B : A, I1, I2
 {
+    alias bar = A.bar;
     override void foo() {}
     override void bar() {}
 }
