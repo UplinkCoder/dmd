cee36b59b3e97c831173b7c0c2bcb15d3db44206
Carsten Schlote <carsten.schlote@gmx.net>
2021-07-17 02:10:00 +0200

    Add .i extension for preprocessed C files to known file extensions (#12879)
    * Add .i extension for preprocessed C files to known set of file extensions
    
    Added .i as extension for preprocessed C files, which can be fed directly
    to DMD. The file is then processed as C source file.
    
    Supporting .i as file exzension allows to preprocess C code with GCC -E and
    store the output to some file for further processing by DMD.
    
    * Update existing Changelog on 'importc' feature.
    
    Add some example on how to use the '.i' suffix instead of the '.c' suffix
    for C files using C includes. Result of C preprocessor can be saved as
    '.i' file and used as input to DMD.
    
    * Add some first simple test for importc with .i files passed on cmdline
    
    * Hide preprocessed C files
    
    * Changed importc test to use .i.in file to fake preprocessor
    
    TODO: Find some preprocessor, which works on Windows and Max targets.
    
    Co-authored-by: Carsten Schlote <carsten.schlote@leica-camera.com>

--- src/dmd/dmodule.d
+++ src/dmd/dmodule.d
@@ -81,6 +81,10 @@ private const(char)[] lookForSourceFile(const char[] filename, const char*[] pat
     const sc = FileName.forceExt(filename, c_ext);
     if (FileName.exists(sc) == 1)
         return sc;
+
+    const si = FileName.forceExt(filename, i_ext);
+    if (FileName.exists(si) == 1)
+        return si;
     scope(exit) FileName.free(sc.ptr);
 
     if (FileName.exists(filename) == 2)
@@ -560,6 +564,7 @@ extern (C++) final class Module : Package
         else if (!FileName.equalsExt(srcfilename, mars_ext) &&
                  !FileName.equalsExt(srcfilename, hdr_ext) &&
                  !FileName.equalsExt(srcfilename, c_ext) &&
+                 !FileName.equalsExt(srcfilename, i_ext) &&
                  !FileName.equalsExt(srcfilename, dd_ext))
         {
 
@@ -1047,8 +1052,9 @@ extern (C++) final class Module : Package
         }
 
         /* If it has the extension ".c", it is a "C" file.
+         * If it has the extension ".i", it is a preprocessed "C" file.
          */
-        if (FileName.equalsExt(arg, c_ext))
+        if (FileName.equalsExt(arg, c_ext) || FileName.equalsExt(arg, i_ext))
         {
             isCFile = true;
 
