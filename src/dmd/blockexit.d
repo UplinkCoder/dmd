d76163d0aeb3a1bb628a399b3fadf64c6b28a3d1
Stefan Koch <uplink.coder@googlemail.com>
2021-08-19 16:35:01 +0200

    Fix Issue 22224

--- src/dmd/blockexit.d
+++ src/dmd/blockexit.d
@@ -107,7 +107,7 @@ int blockExit(Statement s, FuncDeclaration func, bool mustNotThrow)
                         return;
                     }
                 }
-                if (s.exp.type.toBasetype().isTypeNoreturn())
+                if (s.exp.type && s.exp.type.toBasetype().isTypeNoreturn())
                     result = BE.halt;
                 if (canThrow(s.exp, func, mustNotThrow))
                     result |= BE.throw_;
